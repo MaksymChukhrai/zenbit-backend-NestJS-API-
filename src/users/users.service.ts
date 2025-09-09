2a7bfcbf0f90c28d9ec3e3a341da9b7825ad1530
Maksym <m.chukhrai@gmail.com>
2025-09-10 01:34:37 +0300

    =m=

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,25 +1,33 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  constructor(
-    @InjectRepository(User)
-    private usersRepository: Repository<User>,
-  ) {}
+  private users: User[] = []; // Временное хранение в памяти
+  private nextId = 1;
 
-  async create(userData: Partial<User>): Promise<User> {
-    const user = this.usersRepository.create(userData);
-    return this.usersRepository.save(user);
+  create(userData: Partial<User>): Promise<User> {
+    const user = {
+      id: this.nextId++,
+      email: userData.email,
+      password: userData.password,
+      firstName: userData.firstName || null,
+      lastName: userData.lastName || null,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    } as User;
+
+    this.users.push(user);
+    return Promise.resolve(user);
   }
 
-  async findByEmail(email: string): Promise<User | null> {
-    return this.usersRepository.findOne({ where: { email } });
+  findByEmail(email: string): Promise<User | null> {
+    const user = this.users.find((user) => user.email === email) || null;
+    return Promise.resolve(user);
   }
 
-  async findById(id: number): Promise<User | null> {
-    return this.usersRepository.findOne({ where: { id } });
+  findById(id: number): Promise<User | null> {
+    const user = this.users.find((user) => user.id === id) || null;
+    return Promise.resolve(user);
   }
 }
